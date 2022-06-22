67a95dbd40664d61f30ff64a7ef272b96dc160ba
hansikasharma <loveysharma4801@gmail.com>
2022-06-22 10:53:37 +0530

    deployment of my first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+//'http://www.omdbapi.com/?i=tt3896198&apikey=b1f7db95'
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+const Movie1 ={"Title":"Italian Spiderman",
+"Year":"2007",
+"imdbID":"tt2705436",
+"Type":"movie",
+"Poster":"N/A"
 }
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=b1f7db95";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  return (
+   <div className="app">
+   <h1>MovieLand</h1>
+   <div className="search">
+   <input placeholder= "Search for movies"
+   value= {searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+   <img src={SearchIcon} alt= "Search" onClick={()=> searchMovies(searchTerm)}/>
+   </div>
+   {
+	   movies?.length>0?
+	   (<div className="container">
+	   
+	   {movies.map((movie)=>(
+	   <MovieCard movie={movie} />
+	   ))}
+	   </div>
+	   ):
+	   (
+	<div className="Empty"><h2>No movies found</h2>
+	</div>
+	)
+   }
+  
+   </div>
+  
+  );
+};
 
 export default App;
